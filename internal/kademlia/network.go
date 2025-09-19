d62bad34a6dea4bc67231f6a3513cebac7f49671
WTANKW <daniel.wallin35@gmail.com>
2025-09-19 17:19:35 +0200

    added SenFindContactMessage, and fixed 160-bit RPC IDs

--- internal/kademlia/network.go
+++ internal/kademlia/network.go
@@ -1,11 +1,34 @@
 package kademlia
 
 import (
+	"encoding/json"
 	"log"
 	"net"
 	"strconv"
 	"sync"
 )
+
+type MessageType byte
+
+const (
+	PING MessageType = iota
+	PONG
+	FIND_NODE
+	FIND_NODE_RESPONSE
+	STORE
+	FIND_VALUE
+	FIND_VALUE_RESPONSE
+)
+
+// Message represents a Kademlia RPC message with 160-bit RPC ID
+type Message struct {
+	RPCID  *KademliaID `json:"rpcid"`  // 160-bit RPC identifier
+	Type   MessageType `json:"type"`   // Message type
+	Sender Contact     `json:"sender"` // Senders contact info
+	Target *KademliaID `json:"target"` // Target ID for FIND_NODE operation
+	Data   []byte      `json:"data"`   // Data for STORE operations
+	Nodes  []Contact   `json:"nodes"`  // Nodes for FIND_NODE_RESPONSE operation
+}
 
 // Network represents a Kademlia network node with IDP communication capabilities
 type Network struct {
@@ -65,6 +88,18 @@ func Listen(ip string, port int) *Network {
 	return network
 }
 
+// serializeMessage converts a Message struct to JSON bytes for transmission
+func (network *Network) serializeMessage(msg Message) ([]byte, error) {
+	return json.Marshal(msg)
+}
+
+// deserializeMessage converts JSON bytes back to Message struct
+func (network *Network) deserializeMessage(data []byte) (Message, error) {
+	var msg Message
+	err := json.Unmarshal(data, &msg)
+	return msg, err
+}
+
 // listenLoop continuously listens for incoming UDP messages
 func (network *Network) listenLoop() {
 	buffer := make([]byte, 8192) //8KB buffer
@@ -83,27 +118,112 @@ func (network *Network) listenLoop() {
 
 // handleMessage processes incoming messages based on their type
 func (network *Network) handleMessage(data []byte, addr *net.UDPAddr) {
-	// Simple text based protocol for PING(temporary implementation)
-	if string(data) == "PING" {
-		network.handlePing(addr)
+	// Parse the message using proper deserialization
+	msg, err := network.deserializeMessage(data)
+	if err != nil {
+		log.Printf("Error deserializing message: %v\n", err)
 		return
 	}
 
-	//TODO add parsing for other message types
-	log.Printf("Received unknown message: %s\n", string(data))
+	// Update routing table with senders contact info
+	network.RoutingTable.AddContact(msg.Sender)
+
+	// Handle different message types
+	switch msg.Type {
+	case PING:
+		network.handlePing(msg, addr)
+	case PONG:
+		network.handlePong(msg, addr)
+	case FIND_NODE:
+		network.handleFindNode(msg, addr)
+	case FIND_NODE_RESPONSE:
+		network.handleFindNodeResponse(msg, addr)
+	default:
+		log.Printf("Received unknown message type: %d\n", msg.Type)
+	}
 }
 
 // handlePing processes PING requests and sends PONG responses
-func (network *Network) handlePing(addr *net.UDPAddr) {
-	response := []byte("PONG")
-	_, err := network.conn.WriteToUDP(response, addr)
+func (network *Network) handlePing(msg Message, addr *net.UDPAddr) {
+	log.Printf("Received PING from %s (RPC ID: %s)\n", msg.Sender.Address, msg.RPCID.String())
+
+	// Create PONG response with same RPC ID
+	response := Message{
+		RPCID:  msg.RPCID, // Use same RPC ID as request
+		Type:   PONG,
+		Sender: network.RoutingTable.me,
+	}
+
+	responseData, err := network.serializeMessage(response)
+	if err != nil {
+		log.Printf("Error serializing PONG response: %v\n", err)
+		return
+	}
+
+	_, err = network.conn.WriteToUDP(responseData, addr)
 	if err != nil {
 		log.Printf("Error sending PONG response: %v\n", err)
 	}
 }
 
-// SendPingMessage sends a PING message to the specified contact
+// handlePong processes PONG responses
+func (network *Network) handlePong(msg Message, addr *net.UDPAddr) {
+	log.Printf("Received PONG from %s (RPC ID: %s)\n", msg.Sender.Address, msg.RPCID.String())
+	// TODO: Implement response handling for async requests
+}
+
+// handleFindNode processes FIND_NODE requests
+func (network *Network) handleFindNode(msg Message, addr *net.UDPAddr) {
+	log.Printf("Recieved FIND_NODE for target %s from %s\n", msg.Target.String(), msg.Sender.Address)
+
+	// Find closest nodes to the target
+	closestNodes := network.RoutingTable.FindClosestContacts(msg.Target, 20) // k=20
+
+	// Send response
+	response := Message{
+		RPCID:  msg.RPCID, // Match request RPC ID
+		Type:   FIND_NODE_RESPONSE,
+		Sender: network.RoutingTable.me,
+		Nodes:  closestNodes,
+	}
+
+	responseData, err := network.serializeMessage(response)
+	if err != nil {
+		log.Printf("Error serializing FIND_NODE_RESPONSE: %v\n", err)
+		return
+	}
+
+	_, err = network.conn.WriteToUDP(responseData, addr)
+	if err != nil {
+		log.Printf("Error sending FIND_NODE_RESPONSE: %v\n", err)
+	}
+}
+
+// handleFindNodeResponse processes FIND_NODE repsonnses
+func (network *Network) handleFindNodeResponse(msg Message, addr *net.UDPAddr) {
+	log.Printf("Recieved FIND_NODE_RESPONSE with %d nodes from %s\n", len(msg.Nodes), msg.Sender.Address)
+	// TODO: Process the recieved nodes and update routing table
+	// 1. Add the recieved nodes to routing table
+}
+
+// SendPingMessage sends a PING message to the specified contact with 160-bit RPC ID
 func (network *Network) SendPingMessage(contact *Contact) error {
+	// Generate 160-bit RPC ID
+	rpcID := NewRandomKademliaID()
+
+	// Create PING message wit hRPC ID
+	msg := Message{
+		RPCID:  rpcID, //160-bit RPC identifier
+		Type:   PING,
+		Sender: network.RoutingTable.me,
+	}
+
+	//Serialize message
+	msgData, err := network.serializeMessage(msg)
+	if err != nil {
+		return err
+	}
+
 	// Resolve the target address
 	udpAddr, err := net.ResolveUDPAddr("udp", contact.Address)
 	if err != nil {
@@ -111,22 +231,52 @@ func (network *Network) SendPingMessage(contact *Contact) error {
 	}
 
 	// Send PING message via UDP
-	_, err = network.conn.WriteToUDP([]byte("PING"), udpAddr)
-	return err
+	_, err = network.conn.WriteToUDP(msgData, udpAddr)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Sent PING to %s (RPC ID: %s)\n", contact.Address, rpcID.String())
+	return nil
 }
 
-//
-//
-//
+// SendFindContactMessage sends a FIND_NODE message to find contacts close to target ID
+func (network *Network) SendFindContactMessage(contact *Contact, targetID *KademliaID) error {
+	// Generate 160-bit RPC ID
+	rpcID := NewRandomKademliaID()
 
-func (network *Network) SendFindContactMessage(contact *Contact) {
-	// TODO
+	// Create FIND_NODE message
+	msg := Message{
+		RPCID:  rpcID, // 160-bit RPC identifier
+		Type:   FIND_NODE,
+		Sender: network.RoutingTable.me,
+		Target: targetID, // Target node to find
+	}
+
+	// Serialize and send message (similar to SendPingMessage)
+	msgData, err := network.serializeMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	udpAddr, err := net.ResolveUDPAddr("udp", contact.Address)
+	if err != nil {
+		return err
+	}
+
+	_, err = network.conn.WriteToUDP(msgData, udpAddr)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Sent FIND_NODE to %s (RPC ID: %s)\n", contact.Address, targetID.String())
+	return nil
 }
 
 func (network *Network) SendFindDataMessage(hash string) {
-	// TODO
+	// TODO: Implement with proper message format
 }
 
 func (network *Network) SendStoreMessage(data []byte) {
-	// TODO
+	// TODO: Implement with proper message format
 }
