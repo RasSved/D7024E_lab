b50997b6e3ea270d14f7d252de202cc468c9b8e3
Toste1 <94182070+Toste1@users.noreply.github.com>
2025-09-22 15:20:05 +0200

    Update version.go

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -12,8 +12,8 @@ func newVersionCmd() *cobra.Command {
 		Use:   "version",
 		Short: "Print version information",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("Version: %s\n", build.BuildVersion)
-			fmt.Printf("Built at: %s\n", build.BuildTime)
+			fmt.Printf("Version: %s\n", build.BuildVersion) //what version
+			fmt.Printf("Built at: %s\n", build.BuildTime) //when it was built
 		},
 	}
 }
